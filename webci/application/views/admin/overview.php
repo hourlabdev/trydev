9b12a45eb7b79706c685a41bbc058b6ef6a81777
NikoRamadhan <nisasarah02@gmail.com>
2020-05-07 13:19:15 +0700

    first commit

---
+++ webci/application/views/admin/overview.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- tags meta start-->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="HandheldFriendly" content="true">
+	<meta contextmenu="menu-item">
+	<!-- tags meta end -->
+	<title>Tampilan CBT</title>
+	<!-- tags css and plugins start -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/index.css' ?>">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- tags css and plugins end -->
+</head>
+<body>
+	<button onclick="topFunction()" id="myBtn" title="Kembali Ke Atas"><i class="fa fa-chevron-up"></i></button>
+	<!-- tags header start -->
+	<div class="header">
+		<nav>
+			<input type="checkbox" id="check">
+				<label for="check" class="checkbtn"><i class="fa fa-bars"></i></label>
+					<label class="logo">HELLO CBT</label>
+				<ul>
+					<li><a class="active" href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Service</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#join_fb">Feedback</a></li>
+					<li><a href="#"><i class="fa fa-user">Login</i></a></li>
+				</ul>
+		</nav>
+		<div class="banner_img">
+			<div class="banner_text">
+				<h5>School Fashion</h5>
+				<h1>School Fashion 2020</h1>
+				<a href="#join_fb" class="btn_1">Join Now</a>
+			</div>
+		</div>
+	</div>	
+	<!-- tags header end -->
+
+	<!-- tags content start -->
+<div class="content">
+		<div class="explanation">
+			<div class="row">
+				<h4>SMK CBT JAKARTA PROFILE</h4><p>SMK CBT JAKARTA adalah lembaga sekolah yang bergerak di dibidang bisnis manajemen dan teknologi informasi yang telah mendapatkan lisensi SENTOLOP untuk melakukan pendidikan dibidangnya.</p>
+				<a href="#join_fb" class="span_1">Join Now</a>
+			</div>
+		</div>
+		<div class="about">
+			<div class="about_img">
+				<p class="span_2">Image Students Have Graduated	Below This</p>
+						<img src="<?php echo base_url().'assets/img/about_img_1.png' ?>" alt="about_img_1" />
+			</div>
+			<div class="about_paragraph">
+				<h2>How Do You Feel After Graduating From This CBT School?</h2>
+				<p>I feel happy that I am happy because I graduated from CBT school, this is really good, let's join  and be the best student with CBT.</p>
+				<a href="#join_fb" class="span_2">Join Now</a>
+			</div>
+		</div>
+
+		<div class="span_3">
+			<h2>Image CBT Students Graduate<br><hr size="10"></h2>
+		</div>
+		<div class="img_student">
+			<img src="<?php echo base_url().'assets/img/category/category_4.png' ?>" alt="category_4" class="img_student_part">
+			<img src="<?php echo base_url().'assets/img/category/category_5.png' ?>" alt="category_5" class="img_student_part">
+			<img src="<?php echo base_url().'assets/img/category/category_6.png' ?>" alt="category_6" class="img_student_part">
+			<img src="<?php echo base_url().'assets/img/category/category_10.png' ?>"alt="category_10"class="img_student_part">
+			<img src="<?php echo base_url().'assets/img/category/category_7.png' ?>" alt="category_7" class="img_student_part">
+			<img src="<?php echo base_url().'assets/img/category/category_2.png' ?>" alt="category_2" class="img_student_part">
+		</div>
+		<div class="span_4">Excellence of CBT In Below<br/><hr size="10"></div>
+	<div class="excellence_part">
+			<button class="accordion">Section 1</button>
+		<div class="panel">
+		  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+
+			<button class="accordion">Section 2</button>
+		<div class="panel">
+  			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+
+			<button class="accordion">Section 3</button>
+		<div class="panel">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+	</div>	
+	<div class="span_6">Create Account And Join Now</div>
+	<div class="create_section">
+		<div class="bg_img">
+			<<!-- p>Nikmati Layanan Belajar Online Sekarang di CBT dan dapatkan modul-modul pembelajaran yang baru dari CBT sekarang juga</p> -->
+		</div>
+		<div class="form_create">
+			<form action="#" method="post">
+			  <div class="imgcontainer">
+			    <img src="<?php echo base_url().'assets/img/img_avatar2.png' ?>" alt="Avatar" class="avatar">
+			  </div>
+
+			 	<div class="container">
+				    <label for="uname"><b>Username</b></label>
+				    <input type="text" placeholder="Enter Username" name="uname" required>
+
+				    <label for="psw"><b>Password</b></label>
+				    <input type="password" placeholder="Enter Password" name="psw" required>
+				        
+				    <label for="psw"><b>Email Address</b></label>
+				    <input type="text" placeholder="Enter Email Address" name="email" required>
+
+				    <button type="submit" class="btn_create">Create and Join</button>
+				    <label>
+				      <input type="checkbox" checked="checked" name="remember" required><p>Remember me</p>
+				    </label>
+			  	</div>
+			</form>
+		</div>
+	</div>	
+</div>
+	<!-- tags content end -->
+
+	<!-- tags footer start -->
+	<div class="footer">
+		<div class="footer_part">
+			<div class="footer_closing_section">
+				<h3>Closing Section</h3>
+				<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="footer_feedback_section">
+				<h3>FeedBack Section</h3>
+				<form action="#join_fb" method="post" class="form">
+					<input type="email" name="Email" placeholder=" Email Address " onfocus="this.placeholder = ''" onblur="this.placeholder = ' Email Address '" class="form_input"/>
+						<button type="submit" name="submit" class="form_btn" id="join_fb">Subscribe</button><br>
+						<i class="fa fa-facebook"id="icon"></i>
+						<i class="fa fa-twitter" id="icon"></i>
+						<i class="fa fa-youtube" id="icon"></i>
+						<i class="fa fa-google-plus" id="icon"></i>
+				</form>
+			</div>			
+		</div>
+	</div>
+	<div class="footer_copyright">
+		<hr size="0" class="footer_copyright_hr">
+		<div class="footer_copyright_text">
+			<p>Copyright ©
+				<script type="text/javascript">
+				document.write(new Date().getFullYear());
+				</script>
+				 All rights reserved | this website is made with ryuko by <a href="https://www.youtube.com/channel/UCu-USX-k29GUqSvIWok7ciA" target="_blank" class="span_5">labaimaidchan 
+			</p>
+		</div>
+	</div>
+	<!-- tags footer end -->
+
+	<!-- tags js and plugins start -->
+	<script type="text/javascript" src="<?php echo base_url().'assets/js/index.js' ?>"></script>
+	<!-- tags js and plugins end -->
+</body>
+</html>
